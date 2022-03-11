609f945ac1c3c952953c99ed90b633706853f3d6
subhajit-1993 <subhajit.mondal@st.ovgu.de>
2022-03-11 08:04:36 +0100

    stat3

--- sem_project/Assets/RollerAgent.cs
+++ sem_project/Assets/RollerAgent.cs
@@ -260,7 +260,7 @@ public class RollerAgent : Agent
 
     private bool isHeaderWritten = false;
     public void writeStatistics(){
-        string path = "Assets/stats/stat2.txt";
+        string path = "Assets/stats/stat3.txt";
         StreamWriter writer = new StreamWriter(path, true);
         string data = "";
         if(!isHeaderWritten){
